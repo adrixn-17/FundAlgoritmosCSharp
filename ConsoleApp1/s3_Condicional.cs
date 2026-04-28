e3f7fb5d791a614ea122a5bddd2044c422efcf68
Adrian <adriangbz.17@gmail.com>
2026-04-27 22:25:21 -0500

    Agregado: Ejemplos de la Semana 3

---
+++ ConsoleApp1/s3_Condicional.cs
@@ -0,0 +1,11 @@
+// Console.WriteLine("Ingrese su nota");
+// double nota = double.Parse(Console.ReadLine());
+// if (nota >= 12)
+// {
+// Console.WriteLine("Aprobado");
+// }
+
+// else
+// {
+// Console.WriteLine("Desaprobado");
+// }
