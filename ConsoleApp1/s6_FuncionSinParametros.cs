7ec807da2b0d3e33d271e7ad391c51708bf8d3b8
Adrian <adriangbz.17@gmail.com>
2026-05-01 18:59:49 -0500

    Agregado: S6 Ejemplo Alcance de Variables

---
+++ ConsoleApp1/s6_FuncionSinParametros.cs
@@ -0,0 +1,9 @@
+// MostrarMenu();
+
+// void MostrarMenu()
+// {
+//     Console.WriteLine("###### Bienvenido a mi Sistema ######");
+//     Console.WriteLine("Seleccione su Opción: ");
+//     Console.WriteLine("1. Calcular");
+//     Console.WriteLine("2. Salir");
+// }
